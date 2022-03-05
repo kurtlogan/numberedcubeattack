67c740bfa94bd3a5438f504ef534069a510f0e92
Kurt Logan <4375059+kurtlogan@users.noreply.github.com>
2022-03-05 19:11:50 +0000

    wip

--- src/main/scala/io/ddcn/GameScene.scala
+++ src/main/scala/io/ddcn/GameScene.scala
@@ -43,29 +43,84 @@ object GameScene extends Scene[Unit, Unit, Unit]:
       viewModel: Unit
   ): Outcome[SceneUpdateFragment] =
     Outcome(
-      SceneUpdateFragment(world)
+      SceneUpdateFragment(Layer(world))
     )
 
   val world = WorldMap(20, 20, 7, 4)
     .territories
-    .zip(List(RGBA.Coral, RGBA.Orange, RGBA.Plum, RGBA.Crimson, RGBA.Teal, RGBA.White, RGBA.Indigo))
+    .zip(List(RGBA.Coral, RGBA.Orange, RGBA.Plum, RGBA.Crimson, RGBA.Teal, RGBA.SlateGray, RGBA.Indigo))
     .flatMap { case (t, c) =>
       t.zones.flatMap { z =>
-        z.tiles.map { case Tile(x, y) =>
-          polygon(c).moveTo(x * 20, y * 17).moveBy(y % 2 * -10, 0).moveBy(20, 20)
+        z.tiles.flatMap { case t@Tile(x, y) =>
+          List(
+            Some(ViewTile.placeAt(ViewTile.tile(c), x, y)),
+            t.east.map(_ => ViewTile.eastBorder),
+            t.west.map(_ => ViewTile.westBorder),
+            t.southEast.map(_ => ViewTile.eastBorder),
+            t.east.map(_ => ViewTile.eastBorder),
+            t.east.map(_ => ViewTile.eastBorder),
+            t.east.map(_ => ViewTile.eastBorder),
+          ).flatten
         }
       }
     }.toList
 
-  def polygon(color: indigo.shared.datatypes.RGBA) =
-    Shape.Polygon(
-      List(
-        Point(50, 27),
-        Point(40, 20),
-        Point(40, 10),
-        Point(50, 3),
-        Point(60, 10),
-        Point(60, 20)
-      ),
-      Fill.Color(color)
+  object ViewTile {
+
+    val scale: Double = 2
+    val width: Int = (20 * scale).toInt
+    val totalHeight: Int = (17 * scale).toInt
+    val sideHeight: Int = (7 * scale).toInt
+    val lineSize: Int = 5
+
+    def tile(color: RGBA) =
+      Shape.Polygon(
+        List(
+          Point(width / 2, totalHeight),
+          Point(0, totalHeight - ((totalHeight - sideHeight) / 2)),
+          Point(0,  (totalHeight - sideHeight) / 2),
+          Point(width / 2, 0),
+          Point(width, (totalHeight - sideHeight) / 2),
+          Point(width, totalHeight - ((totalHeight - sideHeight) / 2))
+        ),
+        Fill.Color(color)
+      )
+
+    def border(from: Point, to: Point) = Shape.Line(from, to, Stroke(lineSize, RGBA.Black))
+
+    val westBorder = border(
+      Point(0, totalHeight - ((totalHeight - sideHeight) / 2)),
+      Point(0,  (totalHeight - sideHeight) / 2)
     )
+
+    val eastBorder = border(
+      Point(width, (totalHeight - sideHeight) / 2),
+      Point(width, totalHeight - ((totalHeight - sideHeight) / 2))
+    )
+
+    val southWestBorder = border(
+      Point(width / 2, totalHeight),
+      Point(0, totalHeight - ((totalHeight - sideHeight) / 2))
+    )
+
+    val southEastBorder = border(
+      Point(width / 2, totalHeight),
+      Point(width, totalHeight - ((totalHeight - sideHeight) / 2))
+    )
+
+    val northWestBorder = border(
+      Point(0,  (totalHeight - sideHeight) / 2),
+      Point(width / 2, 0),
+    )
+
+    val northEastBorder = border(
+      Point(width / 2, 0),
+      Point(width, (totalHeight - sideHeight) / 2)
+    )
+
+    def placeAt(shape: Shape, x: Int, y: Int): Shape =
+      shape
+        .moveTo(x * width, y * (totalHeight - ((totalHeight - sideHeight) / 2)))
+        .moveBy(y % 2 * -(width / 2), 0) // offset x for every second y
+        .moveBy(40, 40) // add spacing to map
+  }
--- src/main/scala/io/ddcn/NumberedCubeAttack.scala
+++ src/main/scala/io/ddcn/NumberedCubeAttack.scala
@@ -20,7 +20,8 @@ object NumberedCubeAttack extends IndigoGame[Unit, Unit, Unit, Unit]:
     Outcome(
       BootResult.noData(
         GameConfig.default
-          .withViewport(550, 400)
+          .withViewport(1000, 1000)
+          .withClearColor(RGBA.White)
       )
     )
 
